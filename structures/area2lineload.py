bd7e6df680761f04d19de7e1936577f66ac31df7
kowalskiw_via_wsl <kowalskiwojciech986@gmail.com>
2021-12-21 18:17:38 +0100

    error with reading dummy.tsh

--- structures/area2lineload.py
+++ structures/area2lineload.py
@@ -24,11 +24,13 @@ from os import scandir
 import dxfgrabber
 from safir_tools import run_safir, ReadXML, read_in
 import gmsh
+from numpy import interp
 
 # dummy shell template to present area of loading with marked places to insert data
 sh_template = ['Dummy file generated with area2lineload.py\n',
                'Check our github: kowalskiw/fireeng-tools!\n',
-               '     NNODE    50\n',
+               '\n',
+               '     NNODE    ???\n', # here number of elements [-6]
                '      NDIM    3\n',
                '   NDOFMAX    7\n'
                '    NCORES    4\n'
@@ -51,7 +53,7 @@ sh_template = ['Dummy file generated with area2lineload.py\n',
                '   END_FIX\n',
                'NODOFSHELL\n',
                'dummy.tsh\n',
-               '  TRANSLATE    1    4\n',
+               '  TRANSLATE    1    1\n',
                'END_TRANS\n',
                '???',   # here elements [-2]
                'PRECISION 1.0e-3 \n',
@@ -89,6 +91,12 @@ dummy_tsh = [' THICKNESS    0.500\n',
              '  0.7000E-01  0.8000E-01  0.9000E-01  0.1000E+00  0.1100E+00  0.1200E+00  0.1300E+00  0.1400E+00\n',
              '  0.1500E+00  0.1600E+00  0.1700E+00  0.1800E+00  0.1900E+00  0.2000E+00  0.2100E+00  0.2200E+00\n',
              '  0.2300E+00  0.2400E+00  0.2500E+00\n']
+
+
+def distance(a, b): return sqrt(sum([(a[i] - b[i]) ** 2 for i in range(3)]))
+
+
+def is_between(a, c, b): return isclose(distance(a, c) + distance(c, b), distance(a, b))
 
 
 # return list of [shell points tuple, layer name] for each face (3DFACE and POLYFACE) in DXF
@@ -143,13 +151,13 @@ def dxf_import(dxffile_path: str):
 
 
 # require edge points tuple to be order right 1->2->...->n-1->n
-def make_shell(vertices: tuple, element_size: float=None):
+def make_shell(vertices: list[tuple], element_size: float=None):
     gmsh.initialize()
     gmsh.model.add('dummy_shell')
     shell = gmsh.model
 
     # create points
-    pts = [shell.geo.addPoint(*v[1]) for v in vertices]
+    pts = [shell.geo.addPoint(*v) for v in vertices]
 
     # create lines
     lines = []
@@ -160,91 +168,113 @@ def make_shell(vertices: tuple, element_size: float=None):
             lines.append(shell.geo.addLine(pts[-1], pts[0]))
 
     # create surface with quadrilateral mesh type
-    shell.mesh.setRecombine(2, shell.geo.addPlaneSurface([shell.geo.addCurveLoop(lines)]))
+    surf = shell.geo.addPlaneSurface([shell.geo.addCurveLoop(lines)])
     shell.geo.synchronize()
+    shell.mesh.setRecombine(2, surf)
 
     # create mesh
     shell.mesh.setSize(shell.getEntities(), element_size) if element_size else None
     shell.mesh.generate(2)
 
     # get mesh data
-    nodes = shell.mesh.getNodes()
-    elements = shell.mesh.getElements()
+    nodes = []
+    gmsh_nodes = shell.mesh.getNodes()
+    for n in range(len(gmsh_nodes[0])):
+        nodes.append([gmsh_nodes[1][i] for i in [3*n, 3*n+1, 3*n+2]])
+
+    elements = []
+    gmsh_elements = shell.mesh.getElementsByType(3)
+    for e in range(len(gmsh_elements[0])):
+        elements.append([gmsh_elements[1][i] for i in [4*e, 4*e+1, 4*e+2, 4*e+3]])
+
     gmsh.finalize()
 
     return nodes, elements
 
 
 # method to be extended for unstructured meshes
-def find_edges(mesh_nodes: list, geom_points: list):
-    geom_points.append(geom_points[0])
+def find_edges(mesh_nodes: list[list], geom_points: list[tuple]):
+    geom_points = [* geom_points, geom_points[0]]
     edges = []
 
-    def distance(a: tuple, b: tuple):
-        return sqrt(sum([(a[i] - b[i]) ** 2 for i in range(3)]))
-
-    def is_between(a, c, b):
-        return isclose(distance(a, c) + distance(c, b), distance(a, b))
-
-    for n in mesh_nodes:
+    for xtag, coords in enumerate(mesh_nodes):
         for i in range(len(geom_points)-1):
-            edges.append(n[0]) if is_between(geom_points[i], n[1], geom_points[i+1]) else None
+            if is_between(geom_points[i], coords, geom_points[i+1]) and xtag+1 not in edges:
+                edges.append(xtag + 1)
 
     return edges    # list of edge nodes tags
 
 
 # produce and save Safir Structural 3D input file with one shell
-def dummy_area(points: tuple, load: list, n: int):
+def dummy_area(points: list[tuple], load: list, n: int, pathtodir='.'):
     node_template = ['      NODE     ', '  \n']
     element_template = ['      ELEM     ', '   1  \n']
     fix_template = ['     BLOCK     ', '   F0   F0   F0   F0   F0   F0   F0\n']
     load_template = [' DISTRSH    ', '\n']
     dummy_sh = ''.join(sh_template).split('???')
     nodes, elements = make_shell(points)
-    edges = find_edges(elements, nodes)     # edge nodes tags
 
+    edges = find_edges(nodes, points)     # edge nodes tags
+
+    dummy_sh.insert(-6, str(len(nodes)))
     dummy_sh.insert(-5, str(len(elements)))
 
     # define nodes
-    for n in nodes:
-        e_line = node_template
-        [e_line.insert(-1, i) for i in [n[0], n[1][0], n[1][1], n[1][2]]]
-
-        dummy_sh.insert(-4, '   '.join(e_line))
+    for xtag, coords in enumerate(nodes):
+        n_line = node_template.copy()
+        [n_line.insert(-1, str(i)) for i in [xtag+1, *coords]]
+        print(n_line)
+        dummy_sh.insert(-4, '   '.join(n_line))
 
     # fix edge nodes
     for ed_tag in edges:
-        fix_line = fix_template
-        fix_template.insert(-1, ed_tag)
+        fix_line = fix_template.copy()
+        fix_line.insert(-1, str(ed_tag))
 
         dummy_sh.insert(-3, '   '.join(fix_line))
 
     # define elements
-    for e in elements:
-        e_line = element_template
-        [e_line.insert(-1, i) for i in [e[1], e[2][0], e[2][1], e[2][2], e[2][3]]]
+    for xtag, nodes_tags in enumerate(elements):
+        e_line = element_template.copy()
+        [e_line.insert(-1, str(i)) for i in [xtag+1, *nodes_tags]]
 
         dummy_sh.insert(-2, '   '.join(e_line))
 
     # load all elements
     for e in elements:
-        l_line = load_template
-        [l_line.insert(-1, i) for i in [e[1], *load]]
+        l_line = load_template.copy()
+        [l_line.insert(-1, str(i)) for i in [e[1], *load]]
 
         dummy_sh.insert(-1, '   '.join(l_line))
 
-    with open('dummy_{}.in'.format(n), 'w') as d:
+    with open('{}\dummy_{}.in'.format(pathtodir, n), 'w') as d:
         d.write(''.join(dummy_sh))
 
 
 # map element with load
-def map_l2e(edges: list[tuple], lineloads: list[list]):
-    e_load = []  # summary line load per element with given edges
+# points => [[x,y,z] (x3 for start, middle and end)]
+# lineloads => [[p1: list(len=3), r1: list(len=7)], [p2, r2], ... [pn, rn]]
+def map_l2e(points: list, reactions: list[list[list]]):
+    e_load = []  # summary line load per element with given endpoints
+    d1, d2 = (999, 999)
 
-    # for ent in range(7):    # for every DOF
-    #    calculate load at the edges (interpolate 'lineloads' points)
-    #    ld = dvide mean load with double element length
-    #    e_load.append(ld)
+    # find the nearest reaction point
+    to_inter = [None, None]
+    for r in reactions:
+        d_r = distance(r[0], points[1])
+        if d_r < d1:
+            to_inter[0] = r
+
+    # find opposite point
+    for r in reactions:
+        if is_between(to_inter[0], points[1], r[0]):
+            d_r = distance(r[0], points[1])
+            if d_r < d2:
+                to_inter[1] = r
+
+    for dof in range(7):    # for every DOF
+        lineload = interp(0, [-d1, d2], [to_inter[i][dof] for i in (0, 1)])/distance(points[0], points[2])
+        e_load.append(lineload)
 
     return e_load
 
@@ -253,7 +283,7 @@ def map_l2e(edges: list[tuple], lineloads: list[list]):
 def gather_results(pathtodummies='.'):
     reac_data = []
     for s in scandir(pathtodummies):
-        if s.isfile() and 'XML' in s.name:
+        if 'XML' in s.name:
             r = ReadXML(s.path)
             reactions = r.reactions(
                 -1)  # [[node_no, [R(node_no)_dof1,...,R(node_no)_dof7],...,[node_no, [R(node_no)_dof1,...,R(node_no)_dof7]]
@@ -269,13 +299,13 @@ def assign_loads(in_path: str, lineloads: list, function='F1'):
     infile = read_in(in_path)
 
     converted_loads = ['   FUNCTION {}\n'.format(function), '  END_LOAD\n']
-    load_pattern = ' DISTRBEAM    {}    {}    {}    {}\n'
+    load_template = ' DISTRBEAM    {}    {}    {}    {}\n'
 
     # map beam elements to lineloads
     for be in infile.beams:
-        edges = [infile.nodes[n] for n in be[:3]]  # start and end point of beam [[x1,y1,z1],[x2,y2,z2]]
-        elem_loads = map_l2e(edges, lineloads)
-        converted_loads.insert(-1, load_pattern.format(be[0], *elem_loads))
+        points = infile.nodes[be[1:4]]   # start, middle, end point of beam element like [x,y,z]
+        elem_loads = map_l2e(points, lineloads)
+        converted_loads.insert(-1, load_template.format(be[0], *elem_loads))
 
     # make new IN file with line loads
     lloaded = infile.file_lines
@@ -290,27 +320,31 @@ def assign_loads(in_path: str, lineloads: list, function='F1'):
         file.write(''.join(lloaded))
 
 
-def main():
-    calcpath = 'pathtodir'
-    infile = 'pathtoinfile'
-    areas = dxf_import('pathtodxf')  # list of areas [vertices (tuple), loads value (float)]
+def main(pathtodir, pathtoinfile, pathtodxf):
+    calcpath = pathtodir
+    infile = pathtoinfile
+    areas = dxf_import(pathtodxf)  # list of areas [vertices (tuple), loads value (float)]
 
     # create dummy structural files for each area
-    [dummy_area(*areas[i], i) for i in range(len(areas))]
+    [dummy_area(*areas[i], i, pathtodir=pathtodir) for i in range(len(areas))]
 
     # write section thermal (cold) input to calculation directory
-    with open('{}\foo.tem'.format(calcpath), 'w') as tem:
+    with open('{}\\dummy.tsh'.format(calcpath), 'w') as tem:
         tem.writelines(dummy_tsh)
 
     # run mech analyses
     # done
-    [run_safir('dummy_{}.in'.format(i)) for i in range(len(areas))]
+    [run_safir('{}\dummy_{}.in'.format(pathtodir, i)) for i in range(len(areas))]
 
     # postprocess of dummy simulations
     # read reactions from dummy results and save with coordinates of nodes
     # done
-    lineloads = gather_results()
+    lineloads = gather_results(pathtodummies=pathtodir)
 
     # assign line loads to beams from IN file:
     # map2le TO BE DONE
     assign_loads(infile, lineloads)
+
+
+main('D:\\testy\\are2lineload\\test_set\calc\\', 'D:\\testy\\are2lineload\\test_set\\area2line.in',
+     'D:\\testy\\are2lineload\\test_set\\areas.dxf')
